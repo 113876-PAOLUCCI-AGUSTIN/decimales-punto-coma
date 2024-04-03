8ed30985a5fc2c3babda575d4a3605cae5edb258
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-04-03 13:59:33 -0300

    Test created.
    Prueba de que puedo hacer operaciones matemáticas.

--- src/Main.java
+++ src/Main.java
@@ -6,14 +6,18 @@ public class Main {
         // INGRESO CON DECIMAL SEPARADO POR COMA.
         Scanner input = new Scanner(System.in);
         System.out.println("Ingrese un número decimal separado por coma...");
-        Double numeroIngresado01 = input.nextDouble();
-        System.out.println("El número que ingresaste es: "+numeroIngresado01);
+        Double num01 = input.nextDouble();
+        System.out.println("El número que ingresaste es: "+num01);
 
         // INGRESO CON DECIMAL SEPARADO POR PUNTO.
         input = new Scanner(System.in);
         System.out.println("Ingrese un número decimal separado por punto...");
-        Double numeroIngresado02 = Double.parseDouble(input.nextLine());
+        Double num02 = Double.parseDouble(input.nextLine());
         input.close();
-        System.out.println("El número que ingresaste es: "+numeroIngresado02);
+        System.out.println("El número que ingresaste es: "+num02);
+
+        // PRUEBA DE QUE AMBOS NÚMEROS SON FACTIBLES PARA HACER OPERACIONES MATEMÁTICAS.
+        double num03 = Math.pow(num01,num02);
+        System.out.println("El primer número elevado al segundo número es: " +num03);
     }
 }

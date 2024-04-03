583a3f754d2c87ebf99ab372e9e5abf491ddee50
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-04-03 13:53:12 -0300

    Decimales con punto. Creado.

--- src/Main.java
+++ src/Main.java
@@ -8,5 +8,12 @@ public class Main {
         System.out.println("Ingrese un número decimal separado por coma...");
         Double numeroIngresado01 = input.nextDouble();
         System.out.println("El número que ingresaste es: "+numeroIngresado01);
+
+        // INGRESO CON DECIMAL SEPARADO POR PUNTO.
+        input = new Scanner(System.in);
+        System.out.println("Ingrese un número decimal separado por punto...");
+        Double numeroIngresado02 = Double.parseDouble(input.nextLine());
+        input.close();
+        System.out.println("El número que ingresaste es: "+numeroIngresado02);
     }
 }
